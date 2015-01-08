7a93b4de7f7f5165992bd65815053f54411017bf
Tristan Strange <tristan.strange@gmail.com>
2015-01-08 00:33:14 +0000

    Modified saw examples.

--- examples/sawtooth/src/core.cljs
+++ examples/sawtooth/src/core.cljs
@@ -4,7 +4,7 @@
 (def ctx (wa/create-context))        
 
 (def osc (wa/create-oscillator ctx))
-(wa/set! osc :type "sawtooth")
+(wa/set-param! osc :type :sawtooth)
 
 (wa/connect-context-destination osc)
 (wa/start osc)                     
